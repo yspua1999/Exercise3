070c678139e7f245291373fe497005389507b5c9
yspua1999 <56663047+yspua1999@users.noreply.github.com>
2019-12-07 22:00:39 +0800

    done

--- app/src/main/java/com/android/example/exercise3/MainActivity.kt
+++ app/src/main/java/com/android/example/exercise3/MainActivity.kt
@@ -2,9 +2,7 @@ package com.android.example.exercise3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
 import android.widget.*
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,17 +10,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.buttonCalculate).setOnClickListener{
+        findViewById<Button>(R.id.buttonCalculate).setOnClickListener {
             calculatePremium()
         }
 
-        findViewById<Button>(R.id.buttonReset).setOnClickListener{
+        findViewById<Button>(R.id.buttonReset).setOnClickListener {
             reset()
         }
 
     }
 
-    private fun calculatePremium(){
+    private fun calculatePremium() {
         val age = findViewById<Spinner>(R.id.spinnerAge)
         val gender = findViewById<RadioGroup>(R.id.radioGroupGender)
         val isSmoker = findViewById<CheckBox>(R.id.checkBoxSmoker)
@@ -32,80 +30,71 @@ class MainActivity : AppCompatActivity() {
         var genderFee = 0
         var smokerFee = 0
 
-        if(age.selectedItemPosition == 0){
+        if (age.selectedItemPosition == 0) {
             premiumFee = 60
-        }
-
-        else if(age.selectedItemPosition == 1){
+        } else if (age.selectedItemPosition == 1) {
             premiumFee = 70
-            if(gender.checkedRadioButtonId != -1){
+            if (gender.checkedRadioButtonId != -1) {
                 val checked = findViewById<RadioButton>(gender.checkedRadioButtonId)
-               if(checked.text == "Male"){
+                if (checked.text == "Male") {
                     genderFee = 50
                 }
             }
-            if(isSmoker.isChecked){
+            if (isSmoker.isChecked) {
                 smokerFee = 100
             }
-        }
-
-        else if (age.selectedItemPosition == 2){
+        } else if (age.selectedItemPosition == 2) {
             premiumFee = 90
-            if(gender.checkedRadioButtonId != -1){
+            if (gender.checkedRadioButtonId != -1) {
                 val checked = findViewById<RadioButton>(gender.checkedRadioButtonId)
-                if(checked.text == "Male"){
+                if (checked.text == "Male") {
                     genderFee = 100
                 }
             }
-            if(isSmoker.isChecked){
+            if (isSmoker.isChecked) {
                 smokerFee = 150
             }
-        }
-
-        else if (age.selectedItemPosition == 3){
+        } else if (age.selectedItemPosition == 3) {
             premiumFee = 120
-            if(gender.checkedRadioButtonId != -1){
+            if (gender.checkedRadioButtonId != -1) {
                 val checked = findViewById<RadioButton>(gender.checkedRadioButtonId)
-                if(checked.text == "Male"){
+                if (checked.text == "Male") {
                     genderFee = 150
                 }
             }
-            if(isSmoker.isChecked){
+            if (isSmoker.isChecked) {
                 smokerFee = 200
             }
-        }
-
-        else if (age.selectedItemPosition == 4){
+        } else if (age.selectedItemPosition == 4) {
             premiumFee = 150
-            if(gender.checkedRadioButtonId != -1){
+            if (gender.checkedRadioButtonId != -1) {
                 val checked = findViewById<RadioButton>(gender.checkedRadioButtonId)
-                if(checked.text == "Male"){
+                if (checked.text == "Male") {
                     genderFee = 200
                 }
             }
-            if(isSmoker.isChecked){
+            if (isSmoker.isChecked) {
                 smokerFee = 250
             }
-        }
-
-        else if (age.selectedItemPosition == 5){
+        } else if (age.selectedItemPosition == 5) {
             premiumFee = 150
-            if(gender.checkedRadioButtonId != -1){
+            if (gender.checkedRadioButtonId != -1) {
                 val checked = findViewById<RadioButton>(gender.checkedRadioButtonId)
-                if(checked.text == "Male"){
+                if (checked.text == "Male") {
                     genderFee = 200
                 }
             }
-            if(isSmoker.isChecked){
+            if (isSmoker.isChecked) {
                 smokerFee = 300
             }
         }
 
         totalInsurance += premiumFee + genderFee + smokerFee
-        insuranceText.text = (resources.getString(R.string.insurance_premium) + " RM " + totalInsurance.toString())
+        insuranceText.text =
+            (resources.getString(R.string.insurance_premium) + " RM " + totalInsurance.toString())
     }
 
-    private fun reset(){
+    private fun reset() {
         val age = findViewById<Spinner>(R.id.spinnerAge)
         val gender = findViewById<RadioGroup>(R.id.radioGroupGender)
         val isSmoker = findViewById<CheckBox>(R.id.checkBoxSmoker)
